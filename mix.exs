1da1039e6d2cabe0c15afa8a9c3249a05044bf1c
Nicol Acosta <nicolacosta8@gmail.com>
2024-08-09 01:26:25 -0600

    feat: added doc

--- mix.exs
+++ mix.exs
@@ -7,7 +7,16 @@ defmodule TrollBridge.MixProject do
       version: "0.1.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "troll_bridge",
+      description: "Permission verification library with Phoenix/LiveView direct implementation - The troll must give you permission to pass through the bridge.",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      package: package(),
+      source_url: "https://github.com/nicolkill/troll_bridge",
+      docs: [
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -21,7 +30,17 @@ defmodule TrollBridge.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:precuter, "~> 0.1.0"}
+      {:precuter, "~> 0.1.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      name: "troll_bridge",
+      files: ~w(lib .formatter.exs mix.exs README*),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/nicolkill/troll_bridge"}
     ]
   end
 end
